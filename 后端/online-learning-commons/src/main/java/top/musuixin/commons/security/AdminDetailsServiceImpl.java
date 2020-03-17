7b45bda6edb7e3110c087837b5fe5c50c77e95ac
musuixin <fangdangyu@gmail.com>
2020-03-17 22:35:07 +0800

    3.4后端完毕

--- 后端/online-learning-commons/src/main/java/top/musuixin/commons/security/AdminDetailsServiceImpl.java
+++ 后端/online-learning-commons/src/main/java/top/musuixin/commons/security/AdminDetailsServiceImpl.java
@@ -6,8 +6,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import top.musuixin.mzuo.entity.Admin;
-import top.musuixin.mzuo.service.IAdminService;
 
 import java.util.HashSet;
 import java.util.List;
@@ -22,19 +20,20 @@ import java.util.stream.Collectors;
  */
 @Service("adminDetailsService")
 public class AdminDetailsServiceImpl implements UserDetailsService {
-    @Autowired
-    IAdminService adminService;
+//    @Autowired
+//    IAdminService adminService;
 
     @Override
     public UserDetails loadUserByUsername(String adminName) throws UsernameNotFoundException {
-        Admin admin = adminService.findByName(adminName);
-        if (admin == null) {
-            throw new UsernameNotFoundException("该用户不存在");
-        }
-        // 用户权限列表，根据用户拥有的权限标识与如 @PreAuthorize("hasAuthority('sys:menu:view')") 标注的接口对比，决定是否可以调用接口
-        HashSet<String> hashSet = new HashSet<>();
-        hashSet.add("admin");
-        List<GrantedAuthority> grantedAuthorities = hashSet.stream().map(GrantedAuthorityImpl::new).collect(Collectors.toList());
-        return new JwtUserDetails(admin.getAdminName(), admin.getAdminPassword(), admin.getAdminSalt(), grantedAuthorities);
+//        Admin admin = adminService.findByName(adminName);
+//        if (admin == null) {
+//            throw new UsernameNotFoundException("该用户不存在");
+//        }
+//        // 用户权限列表，根据用户拥有的权限标识与如 @PreAuthorize("hasAuthority('sys:menu:view')") 标注的接口对比，决定是否可以调用接口
+//        HashSet<String> hashSet = new HashSet<>();
+//        hashSet.add("admin");
+//        List<GrantedAuthority> grantedAuthorities = hashSet.stream().map(GrantedAuthorityImpl::new).collect(Collectors.toList());
+//        return new JwtUserDetails(admin.getAdminName(), admin.getAdminPassword(), admin.getAdminSalt(), grantedAuthorities);
+        return null;
     }
 }
--- 后端/online-learning-monitor/src/test/java/top/musuixin/onlinelearningmonitor/OnlineLearningMonitorApplicationTests.java
+++ 后端/online-learning-monitor/src/test/java/top/musuixin/onlinelearningmonitor/OnlineLearningMonitorApplicationTests.java
@@ -1,13 +1,13 @@
-package top.musuixin.onlinelearningmonitor;
-
-import org.junit.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class OnlineLearningMonitorApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package top.musuixin.onlinelearningmonitor;
+//
+//import org.junit.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class OnlineLearningMonitorApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
